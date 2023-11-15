c955abd57ab80c1e4a57bf2cbc93ce6f351bb99d
rizilmaharjan <rizilmaharjan@gmail.com>
2023-11-15 20:31:27 +0545

    Feature: Add upload image functionality

--- fe/src/pages/Profile.tsx
+++ fe/src/pages/Profile.tsx
@@ -1,6 +1,19 @@
 import { useForm } from "react-hook-form";
 import { useAppSelector } from "../redux/hooks";
+import { useRef, useState, useEffect } from "react";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 export default function Profile() {
+  const fileRef = useRef<HTMLInputElement | null>(null);
+  const [image, setImage] = useState<File | undefined>(undefined);
+  const [imagePercent, setImagePercent] = useState(0);
+  const [imageError, setImageError] = useState(false);
+  const [formData, setFormData] = useState({});
   const {currentUser} = useAppSelector(state=>state.user)
   const {
     register,
@@ -14,16 +27,72 @@ export default function Profile() {
       password: "",
     },
   });
+  useEffect(() => {
+    if (image) {
+      handleFileUpload(image);
+    }
+  }, [image]);
+  const handleFileUpload = async (image:File) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + image.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, image);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(progress);
+        setImagePercent(Math.round(progress));
+      },
+      (error) => {
+        setImageError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setFormData({
+            ...FormData,
+            profilePicture: downloadURL,
+          });
+        });
+      }
+    );
+  };
   return (
     <>
       <div className="p-3 max-w-lg mx-auto">
         <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
         <form className="flex flex-col gap-6">
+        <input
+            type="file"
+            ref={fileRef}
+            hidden
+            accept="image/*"
+            onChange={(e) => setImage(e.target.files?.[0])}
+          />
+          {/* firebase storage rules */}
+          {/* allow read;
+      allow write: if
+      request.resource.size < 4 * 1024 * 1024 &&
+      request.resource.contentType.matches('image/.*') */}
           <img
             className="h-24 w-24 rounded-full self-center cursor-pointer"
             src={currentUser?.profilePicture}
             alt="profile-picture"
+            onClick={() => fileRef.current?.click()}
           />
+           <p className="text-sm self-center">
+            {imageError ? (
+              <span className="text-red-700">Error uploading image (file size must be less than 4MB) </span>
+            ) : imagePercent > 0 && imagePercent < 100 ? (
+              <span className="text-slate-700">{`uploading: ${imagePercent} %`}</span>
+            ) : imagePercent === 100 ? (
+              <span className="text-green-700">Image uploaded successfully</span>
+            ) : (
+              ""
+            )}
+          </p>
+
           <input
             type="text"
             placeholder="Username"
