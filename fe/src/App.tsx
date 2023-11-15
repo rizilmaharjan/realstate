a12d71dae8082eec27260d0b71f54acda69d1a21
rizilmaharjan <rizilmaharjan@gmail.com>
2023-11-15 19:37:37 +0545

    Feature: Create profile route private

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -5,6 +5,7 @@ import SignUp from "./pages/SignUp"
 import Profile from "./pages/Profile"
 import SignIn from "./pages/SignIn"
 import Navbar from "./components/Navbar"
+import PrivateRoute from "./components/PrivateRoute"
 export default function App() {
   return (
     <>
@@ -15,7 +16,10 @@ export default function App() {
           <Route path="/about" element={<About />} />
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
+          <Route element={<PrivateRoute />}>
+
           <Route path="/profile" element={<Profile />} />
+          </Route>
         </Routes>
       </BrowserRouter>
 
---
+++ fe/src/components/PrivateRoute.tsx
@@ -0,0 +1,6 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useAppSelector } from "../redux/hooks";
+export default function PrivateRoute() {
+    const {currentUser} = useAppSelector(state=>state.user)
+    return currentUser ? <Outlet /> : <Navigate to="/sign-in" />
+}
