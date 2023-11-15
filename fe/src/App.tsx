7f71d0dd8879387f491ea81be62bc7d9d63839a6
rizilmaharjan <rizilmaharjan@gmail.com>
2023-11-15 13:58:26 +0545

    Feature: Add routes and create pages

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,7 +1,21 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import SignUp from "./pages/SignUp"
+import Profile from "./pages/Profile"
+import SignIn from "./pages/SignIn"
 export default function App() {
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
 
     </>
   )
