dfda276e5c4e69485eb2cf50e017466801bbf0cf
rizilmaharjan <rizilmaharjan@gmail.com>
2023-11-15 14:47:36 +0545

    Feature: Add database connection

--- be/src/index.ts
+++ be/src/index.ts
@@ -2,9 +2,11 @@ import 'dotenv/config'
 import express from "express"
 import cors from "cors"
 import bodyParser from "body-parser"
+import { connectDB } from '../config/dbConnect'
 
 const PORT = process.env.PORT || 8000
 
+connectDB()
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
