9c796bda58182dbb272ace049bd9bab847ee0bf0
rizilmaharjan <rizilmaharjan@gmail.com>
2024-03-13 12:07:53 +0545

    Feature: Add dockerization

--- be/src/index.ts
+++ be/src/index.ts
@@ -1,38 +1,30 @@
-import 'dotenv/config'
-import express from "express"
-import cors from "cors"
-import bodyparser from "body-parser"
-import { connectDB } from '../config/dbConnect'
-import cookieParser from 'cookie-parser'
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import bodyparser from "body-parser";
+import { connectDB } from "../config/dbConnect";
+import cookieParser from "cookie-parser";
 
+import authRoutes from "./auth/index";
+import userRoutes from "./user/index";
+import listingRoutes from "./listing/index";
 
-import authRoutes from "./auth/index"
-import userRoutes from "./user/index"
-import listingRoutes from "./listing/index"
+const PORT = process.env.PORT || 8000;
 
-
-const PORT = process.env.PORT || 8000
-
-connectDB()
-const app = express()
-app.use(cors({
+connectDB();
+const app = express();
+app.use(
+  cors({
     credentials: true,
-    origin: "http://localhost:5173"
-}))
-app.use(bodyparser.json())
-app.use(cookieParser())
-app.use("/api", authRoutes())
-app.use("/api", userRoutes())
-app.use("/api", listingRoutes())
+    origin: "http://localhost:5173",
+  })
+);
+app.use(bodyparser.json());
+app.use(cookieParser());
+app.use("/api", authRoutes());
+app.use("/api", userRoutes());
+app.use("/api", listingRoutes());
 
-
-
-
-
-
-
-
-
-app.listen(PORT,()=>{
-    console.log(`Server started on http://localhost:8000`);
-})
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:8000`);
+});
